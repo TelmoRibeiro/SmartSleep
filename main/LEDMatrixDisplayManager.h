1f12512de6cd8ef6657e8b0421c170db98804ade
Telmo Ribeiro <61145755+TelmoRibeiro@users.noreply.github.com>
2026-04-10 16:05:16 +0100

    #4 - LEDMatrixDisplayManager describes RED_HEX_COLOUR as constexpr [previously was a macro]

--- main/LEDMatrixDisplayManager.h
+++ main/LEDMatrixDisplayManager.h
@@ -4,10 +4,10 @@
 #include "ArduinoGraphics.h"
 #include "Arduino_LED_Matrix.h"
 
-#define RED_HEX_COLOUR 0xFFFFFFFFU
-
 class LEDMatrixDisplayManager : public IDisplayManager {
 private:
+    static constexpr uint32_t RED_HEX_COLOUR = 0xFFFFFFFFU;
+
     /** @brief internal state of a LEDMatrixDisplayManager instance
      */
     struct LEDMatrixDisplayManagerContext {
