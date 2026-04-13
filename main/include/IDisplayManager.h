0761ecfe895a9bbdb4688b133755608316cf041c
Telmo André Azevedo Ribeiro <61145755+TelmoRibeiro@users.noreply.github.com>
2026-04-13 12:50:48 +0100

    Display Manager (#7)
    * SS2 - IDisplayManager virtual interface generated with default destructor method
    
    * SS2 - IDisplayMangerTypes generated with TextFontSize & ScrollDirection enum classes
    
    * SS2 - IDisplayManager references methods for generic display behaviour [start/clear/play/update/isDone]
    
    * SS2 - IDisplayManager references showStaticText & showScrollableText methods
    
    * SS2 - IDisplayManager references setters & getters methods for text configuration [textFontSize/textColour/scrollSpeed]
    
    * SS2 - IDisplayManager references setPixel method for singular pixel manipulation
    
    * SS2 - LEDMatrixDisplayManager extends IDisplayManager for Arduino R4 WiFi's LED matrix display
    
    * SS2 - main describes a leftward scrollable 'Hello World' text
    
    * SS2 - gitignore references IDE-specific & claude-specific files

---
+++ main/include/IDisplayManager.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include "IDisplayManagerTypes.h"
+
+class IDisplayManager {
+public:
+    /** @brief destroys the display manager
+     */
+    virtual ~IDisplayManager(void) = default;
+
+    /** @brief starts the display
+     */
+    virtual void start(void) = 0;
+
+    /** @brief clears the display
+     */
+    virtual void clear(void) = 0;
+
+    /** @brief plays the text sequence on the display
+     *  @param shouldLoop whether to loop the text sequence
+     */
+    virtual void play(bool shouldLoop) = 0;
+
+    /** @brief updates the display manager -- to be called each iteration of the main loop
+     */
+    virtual void update(void) = 0;
+
+    /** @brief checks whether the animation sequence has finished playing
+     *  @return true if the animation sequence has finished playing, false otherwise
+     */
+    virtual bool isDone(void) const = 0;
+
+    /** @brief show static text on the display
+     *  @param text the static text to display
+     *  @param x    the x-coordinate for the text
+     *  @param y    the y-coordinate for the text
+    */
+    virtual void showStaticText(const char* text, int32_t x, int32_t y) = 0;
+
+   /** @brief show scrollable text on the display
+    *  @param text            the scrollable text to display
+    *  @param scrollDirection the direction of the scroll
+    *  @param x               the x-coordinate for the text
+    *  @param y               the y-coordinate for the text
+    */ 
+    virtual void showScrollableText(const char* text, ScrollDirection scrollDirection, int32_t x, int32_t y) = 0;
+
+    /** @brief sets the font size for the text shown on the display
+     *  @param textFontSize the font size for the text shown on the display
+     */
+    virtual void setTextFontSize(TextFontSize textFontSize) = 0;
+
+    /** @brief gets the font size from the text shown on the display
+     *  @return the font size from the text shown on the display
+     */
+    virtual TextFontSize getTextFontSize(void) const = 0;
+
+    /** @brief sets the colour for the text shown on the display
+     *  @param textColour the colour for the text shown on the display
+     */
+    virtual void setTextColour(uint32_t textColour) = 0;
+
+    /** @brief gets the colour from the text shown on the display
+     *  @return the colour from the text shown on the display
+     */
+    virtual uint32_t getTextColour(void) const = 0;
+
+    /** @brief sets the scroll speed for the text shown on the display
+     *  @param scrollSpeedMS the scroll speed in milliseconds for the text shown on the display
+     */
+    virtual void setScrollSpeedMS(uint32_t scrollSpeedMS) = 0;
+
+    /** @brief gets the scroll speed from the text shown on the display
+     *  @return the scroll speed in milliseconds from the text shown on the display
+     */
+    virtual uint32_t getScrollSpeedMS(void) const = 0;
+
+    /** @brief sets the state of a singular pixel on the display
+     *  @param row  the row of the pixel to be set
+     *  @param col  the column of the pixel to be set
+     *  @param isOn the state of the pixel to be set
+     */
+    virtual void setPixel(uint8_t row, uint8_t col, bool isOn) = 0;
+};
